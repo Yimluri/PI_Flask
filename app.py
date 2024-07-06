14f716ed263930b10a80e4f776d8640a7bba46be
Yimluri <jlugoarr04@gmail.com>
2024-07-05 22:54:04 -0600

    registroMed

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask,request,jsonify,render_template
+from flask import Flask,request,jsonify,render_template,flash,url_for,redirect
 
-app = Flask (__name__)
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'proyectoi1'
+app.config['MYSQL_PORT'] = 3307
+
+mysql = MySQL(app)
+app.secret_key = 'mysecretkey'
 
 #ruta simple
 
@@ -39,6 +49,22 @@ def test():
 @app.route('/respuestas')
 def respuestas():
    return render_template('respuestas.html')
+
+@app.route('/guardarMedico',methods=['POST'])
+def guardarMedico():
+
+    if request.method== 'POST':
+         FNombre=request.form['txtNombre']
+         FCedula=request.form['txtCedula']
+         FTelefono=request.form['txtTelefono']
+         FCorreo=request.form['txtCorreo']
+         FPassword=request.form['txtPassword']
+         
+         cursor = mysql.connection.cursor()
+         cursor.execute('insert into table_med(nombre,cedula,telefono,correo,password) values(%s,%s,%s,%s,%s)', (FNombre,FCedula,FTelefono,FCorreo,FPassword))
+         mysql.connection.commit()
+         flash('Registro guardado correctamente')
+         return redirect(url_for('registroMedico'))
 
 
 if __name__ == '__main__':
